3176f603b05e0ffbe261142da7e330eb35d050b3
Ryan Tse <ryantse@verizon.net>
2016-02-08 17:21:42 -0500

    Created new auto branch (for autonomous period code) and auto code stub

---
+++ src/main/java/org/usfirst/frc/team449/robot/commands/Autonomous.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team449.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * <code>CommandGroup</code> for running the autonomous period
+ * <code>Command</code>s
+ * 
+ * @author Ryan Tse <ryantse100@gmail.com>
+ * @since 2016-02-08
+ */
+public class Autonomous extends CommandGroup {
+
+	/**
+	 * Instantiate a new <code>Autonomous</code>
+	 */
+	public Autonomous() {
+	}
+}
