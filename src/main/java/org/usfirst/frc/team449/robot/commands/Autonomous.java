8607b35aae2b4ddc619a04bb107f953f277a0327
Ryan Tse <ryantse@verizon.net>
2016-02-11 18:25:55 -0500

    Wrote pseudocode for one strategy in Autonomous CommandGroup

--- src/main/java/org/usfirst/frc/team449/robot/commands/Autonomous.java
+++ src/main/java/org/usfirst/frc/team449/robot/commands/Autonomous.java
@@ -11,9 +11,24 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class Autonomous extends CommandGroup {
 
+	private DefenseType defenseType;
 	/**
 	 * Instantiate a new <code>Autonomous</code>
 	 */
-	public Autonomous() {
+	public Autonomous(DefenseType defenseType) {
+		this.defenseType = defenseType;
+		executeStrategy0();
+	}
+
+	private void executeStrategy0() {
+		// TODO drive certain distance to the defense
+		// TODO if something in front, stop
+		// TODO set the breach arm in the right position
+		// TODO breach
+		// TODO about face
+		// TODO eject ball
+		// TODO drive a certain distance
+		// TODO breach
+		// TODO drive a certain distance
 	}
 }
---
+++ src/main/java/org/usfirst/frc/team449/robot/commands/DefenseType.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team449.robot.commands;
+
+public enum DefenseType {
+	PORTCULLIS, CHEVAL_DE_FRISE, MOAT, RAMPARTS, DRAWBRIDGE, SALLY_PORT, ROCK_WALL, ROUGH_TERRAIN, LOW_BAR;
+}
