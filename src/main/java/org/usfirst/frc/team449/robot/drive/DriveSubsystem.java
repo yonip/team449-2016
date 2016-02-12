5fdca702f5b326efe7a9395d0ddbcd84b9d2ca49
Ryan Tse <ryantse@verizon.net>
2016-02-12 16:47:52 -0500

    Merged alpha into auto

--- src/main/java/org/usfirst/frc/team449/robot/drive/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc/team449/robot/drive/DriveSubsystem.java
@@ -6,5 +6,4 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * a generic Drive subsystem outline, also used as a generic in Robot
  */
 public abstract class DriveSubsystem extends Subsystem {
-
 }
