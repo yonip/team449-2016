4bdd84129b26e9e911004ba30f8bae6bd44d9d8e
yonip <yoni.pedersen@gmail.com>
2016-02-20 13:43:11 -0500

    Add buttons and bind them to commands

--- src/main/java/org/usfirst/frc/team449/robot/OI.java
+++ src/main/java/org/usfirst/frc/team449/robot/OI.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc.team449.robot;
 
 import org.usfirst.frc.team449.robot.mechanism.breach.commands.BreachChivald;
+import org.usfirst.frc.team449.robot.mechanism.breach.commands.BreachPortcullis;
 import org.usfirst.frc.team449.robot.mechanism.breach.commands.BreachStowed;
+import org.usfirst.frc.team449.robot.mechanism.intake.commands.IntakeDown;
 import org.usfirst.frc.team449.robot.mechanism.intake.commands.IntakeIn;
 import org.usfirst.frc.team449.robot.mechanism.intake.commands.IntakeOut;
+import org.usfirst.frc.team449.robot.mechanism.intake.commands.IntakeUp;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -21,6 +24,7 @@ public class OI/*vey*/ {
 	private Joystick intakeJoystick;
 	
 	private Joystick gamecube;
+	private double db = 0.01;
 
     public OI(OIMap map) {
         this.map = map;
@@ -33,26 +37,45 @@ public class OI/*vey*/ {
 
         Button intakeIn = new JoystickButton(gamecube, map.INTAKE_IN);
         Button intakeOut = new JoystickButton(gamecube, map.INTAKE_OUT);
-        Button intakeToggle = new JoystickButton(gamecube, 8);
+        //Button intakeToggle = new JoystickButton(gamecube, 8);
+        Button intakeUp = new JoystickButton(gamecube, 8);
+        Button intakeDown = new JoystickButton(gamecube, 7);
         Button breachChival = new JoystickButton(gamecube, 1);
         Button breachPortcullis = new JoystickButton(gamecube, 3);
         Button breachClose = new JoystickButton(gamecube, 10);
+        Button cameraToggle = new JoystickButton(gamecube, 9); // new
+        Button driveStraightVel = new JoystickButton(gamecube, 6); // new
+        //Button driveStraightPos = new JoystickButton(gamecube, 5); // new        
 
         intakeIn.toggleWhenPressed(new IntakeIn());
         intakeOut.whileHeld(new IntakeOut());
+        // TODO intakeToggle command
+        intakeUp.whenPressed(new IntakeUp());
+        intakeDown.whenPressed(new IntakeDown());
         
         breachChival.whenPressed(new BreachChivald());
+        breachPortcullis.whenPressed(new BreachPortcullis()); // new
         breachClose.whenPressed(new BreachStowed());
+        
+//        cameraToggle.whenActive(new );
     }
 
 	public double getDriveAxisLeft() {
 		//return this.leftDriveJoystick.getAxis(Joystick.AxisType.kY);
-		return this.gamecube.getRawAxis(map.LEFT_DRIVE_STICK);
+		double ret= -this.gamecube.getRawAxis(map.LEFT_DRIVE_STICK);
+		if (Math.abs(ret) < db) {
+			return 0;
+		}
+		return ret;
 	}
 
 	public double getDriveAxisRight() {
 		//return this.rightDriveJoystick.getAxis(Joystick.AxisType.kY);
-		return this.gamecube.getRawAxis(map.RIGHT_DRIVE_STICK);
+		double ret = -this.gamecube.getRawAxis(map.RIGHT_DRIVE_STICK);
+		if (Math.abs(ret) < db) {
+			return 0;
+		}
+		return ret;
 	}
 
 	public boolean isDriveStraightMode() {
