da949a586642b2ebc8281d108686edbc97e27db7
yonip <yoni.pedersen@gmail.com>
2016-03-15 16:42:53 -0400

    Command using BangBang to drive a given distance

--- src/main/java/org/usfirst/frc/team449/robot/drive/tank/commands/DriveDistance.java
+++ src/main/java/org/usfirst/frc/team449/robot/drive/tank/commands/DriveDistance.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc.team449.robot.drive.tank.commands;
 
 import org.usfirst.frc.team449.robot.Robot;
+import org.usfirst.frc.team449.robot.drive.tank.TankDriveMap;
 import org.usfirst.frc.team449.robot.drive.tank.TankDriveSubsystem;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * <code>Command</code> for driving a certain distance (for use in auto period)
@@ -16,7 +19,8 @@ public class DriveDistance extends Command {
 
 	private double distance;
 
-	public DriveDistance(double distance) {
+	public DriveDistance(double distance, double timeout) {
+		super(timeout);
 		requires(Robot.drive);
 		this.distance = distance;
 	}
@@ -24,25 +28,35 @@ public class DriveDistance extends Command {
 	@Override
 	protected void initialize() {
 		System.out.println("DriveDistance init");
+		TankDriveSubsystem drive = (TankDriveSubsystem) (Robot.drive);
+		drive.reset();
 	}
 
 	@Override
 	protected void execute() {
-		((TankDriveSubsystem) Robot.drive).driveDistance(distance);
+		TankDriveSubsystem drive = (TankDriveSubsystem) (Robot.drive);
+		TankDriveMap map = (TankDriveMap) (drive.map);
+		drive.setThrottle(-map.SPEED*.5, map.SPEED*.5);
 	}
 
 	@Override
 	protected boolean isFinished() {
-		return ((TankDriveSubsystem) Robot.drive).getDriveDistanceDone();
+		TankDriveSubsystem drive = (TankDriveSubsystem) (Robot.drive);
+		SmartDashboard.putNumber("Distance", drive.getDistance());
+		return drive.getDistance() > this.distance;
 	}
 
 	@Override
 	protected void end() {
 		System.out.println("DriveDistance end");
+		TankDriveSubsystem drive = (TankDriveSubsystem) (Robot.drive);
+		drive.setThrottle(0, 0);
 	}
 
 	@Override
 	protected void interrupted() {
 		System.out.println("DriveDistance interupted");
+		TankDriveSubsystem drive = (TankDriveSubsystem) (Robot.drive);
+		drive.setThrottle(0, 0);
 	}
 }
