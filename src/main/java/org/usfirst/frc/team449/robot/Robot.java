549c7363f842bdad3c5f9078c0ebc97d6d3ed641
yonip <yoni.pedersen@gmail.com>
2016-03-15 17:01:11 -0400

    Add autochooser and running auto commands

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -1,10 +1,15 @@
 package org.usfirst.frc.team449.robot;
 
 import org.json.JSONObject;
+import org.usfirst.frc.team449.robot.commands.Auto;
+import org.usfirst.frc.team449.robot.commands.AutoDrive;
+import org.usfirst.frc.team449.robot.commands.AutoDriveIntakeUp;
+import org.usfirst.frc.team449.robot.commands.AutoPortcullis;
 import org.usfirst.frc.team449.robot.commands.DefenseType;
 import org.usfirst.frc.team449.robot.drive.DriveSubsystem;
 import org.usfirst.frc.team449.robot.drive.tank.TankDriveMap;
 import org.usfirst.frc.team449.robot.drive.tank.TankDriveSubsystem;
+import org.usfirst.frc.team449.robot.drive.tank.commands.DriveDistance;
 import org.usfirst.frc.team449.robot.mechanism.breach.BreachMap;
 import org.usfirst.frc.team449.robot.mechanism.breach.BreachSubsystem;
 import org.usfirst.frc.team449.robot.mechanism.intake.IntakeMap;
@@ -13,7 +18,10 @@ import org.usfirst.frc.team449.robot.vision.VisionSubsystem;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * the class tying all of the components of the robot together.
@@ -50,6 +58,10 @@ public class Robot extends IterativeRobot {
 	 * which obstacle the robot will try to breach during auto
 	 */
 	public static DefenseType autoDefenseType = DefenseType.PORTCULLIS;
+	
+	private Command autonomousCommand;
+	
+	SendableChooser autoChooser;
 
 	/**
 	 * Robot-wide initialization code should go here.
@@ -72,6 +84,12 @@ public class Robot extends IterativeRobot {
 			breach = new BreachSubsystem(new BreachMap(cfg));
 			vision = new VisionSubsystem();
 			oi = new OI(new OIMap(cfg));
+			autoChooser = new SendableChooser();
+			autoChooser.addDefault("nothing", new Auto());
+			autoChooser.addObject("Drive dist", new AutoDrive(190, 4.5));
+			autoChooser.addObject("Drive Port", new AutoPortcullis(4.5));
+			autoChooser.addObject("Drive Intake Up)", new AutoDriveIntakeUp(190, 4.5));
+			SmartDashboard.putData("Auto chooser", autoChooser);
 		} catch (Exception e) {
 			String s = e.getMessage();
 			StackTraceElement[] arr = e.getStackTrace();
@@ -90,16 +108,23 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousInit() {
-
+		autonomousCommand = (Command) autoChooser.getSelected();
+        if (autonomousCommand != null); autonomousCommand.start();
 	}
 
 	@Override
 	public void teleopInit() {
+		if (autonomousCommand != null) autonomousCommand.cancel();
 	}
 
 	@Override
 	public void testInit() {
 
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
 	}
 
 	@Override
