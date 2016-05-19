c519ca5eb6ffc1f4f4aa6bd16fbf06e0e128ee05
Ryan Tse <ryantse100@gmail.com>
2016-05-19 14:20:20 -0400

    Wrote documentation for BreachChivald

--- src/main/java/org/usfirst/frc/team449/robot/mechanism/breach/commands/BreachChivald.java
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/breach/commands/BreachChivald.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team449.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * Command to set the breach arm to the state for breaching the chivald de fries
+ * <code>Command</code> that sets the breach arm to the state for breaching the chivald de fries
  * 
  * @author Ryan Tse <ryantse100@gmail.com>
  * @since 2016-02-07
@@ -20,27 +20,43 @@ public class BreachChivald extends Command {
 		requires(Robot.breach);
 	}
 
+	/**
+	 * Initialize the <code>Command</code>
+	 */
 	@Override
 	protected void initialize() {
 		System.out.println("BreachChivald init");
 	}
 
+	/**
+	 * Sets the solenoids to the chivald (middle, (F, R)) position
+	 */
 	@Override
 	protected void execute() {
 		Robot.breach.setBackSolenoidForward();
 		Robot.breach.setFrontSolenoidReverse();
 	}
 
+	/**
+	 * Returns <code>true</code> to the Scheduler, as the <code>Command</code>
+	 * is complete as soon as the <code>execute</code> is run, firing the solenoids.
+	 */
 	@Override
 	protected boolean isFinished() {
 		return true;
 	}
 
+	/**
+	 * End the <code>Command</code>
+	 */
 	@Override
 	protected void end() {
 		System.out.println("BreachChivald end");
 	}
 
+	/**
+	 * Interrupt the <code>Command</code>
+	 */
 	@Override
 	protected void interrupted() {
 		System.out.println("BreachChivald interupted");
