1a7dea45d08b7a43cce9e8093a8b9037207f7cac
Ryan Tse <ryantse100@gmail.com>
2016-05-19 13:43:27 -0400

    Wrote documentation for MechanismSubsystem

--- src/main/java/org/usfirst/frc/team449/robot/mechanism/MechanismSubsystem.java
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/MechanismSubsystem.java
@@ -4,9 +4,17 @@ import org.usfirst.frc.team449.robot.MappedSubsystem;
 import org.usfirst.frc.team449.robot.RobotMap;
 
 /**
- * Outline class for any mechanisms on the robot
+ * <p>
+ * This is the base class for all mechanisms on the robot. It extends
+ * {@link #org.usfirst.frc.team449.robot.RobotMap} and contains a
+ * {@link #org.usfirst.frc.team449.robot.mechanism.MechanismMap to
+ * hold constants.
+ * </p>
  */
 public abstract class MechanismSubsystem extends MappedSubsystem {
+	/**
+	 * Instantiates a new <code>MechanismSubsystem</code> with a <code>RobotMap</code>
+	 */
 	public MechanismSubsystem(RobotMap map) {
 		super(map);
 	}
